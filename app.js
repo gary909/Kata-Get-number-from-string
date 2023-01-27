392a4d42c2b7639d79fdcb4c87437a2d6a12f273
Gary White <garywhite61@hotmail.com>
2023-01-27 10:59:09 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function getNumberFromString(s) {
+  return 0;
+}
+
+console.log(getNumberFromString("1")); // 1
+console.log(getNumberFromString("123")); // 123
+console.log(getNumberFromString("this is number: 7")); // 7
