7c91822432ed52aea7bbabf5ef098e9feee3bdcf
Gary White <garywhite61@hotmail.com>
2023-01-27 11:35:13 +0100

    code working

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 function getNumberFromString(s) {
-  return 0;
+    let myStr = s.split('');
+    let myArr = [];
+    for (let i = 0; i < myStr.length; i++){
+        if(isNaN(s[i])){
+            // do nothing
+        } else {
+            myArr.push(myStr[i]);
+        }
+    }
+    // let myStr2 = myArr.toString();
+    const result = myArr.filter(myArr => myArr.trim().length > 0);
+    let myStr2 = result.join('');
+    // let mystr2 = myArr.replace(/\s/g, '');
+    return myStr2;
 }
 
 console.log(getNumberFromString("1")); // 1
